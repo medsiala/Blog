03906ef015fe6c65b791ad1f8b9e71a957d53466
sialamed <siala.mohamed.ms@gmail.com>
2020-06-02 13:39:09 +0200

    responsive mobile header

--- src/assets/javascripts/topbar.js
+++ src/assets/javascripts/topbar.js
@@ -1,1 +1,47 @@
-console.log("tolbar");
+const iconMobile = document.querySelector(".header-menu-icons");
+const headerMenu = document.querySelector(".header-menu");
+let isMenuOpen = false;
+let mobileMenuDOM;
+
+const creatMobileMenu = () => {
+  mobileMenuDOM = document.createElement("div");
+  mobileMenuDOM.classList.add("mobile-menu");
+  mobileMenuDOM.addEventListener("click", (event) => {
+    event.stopPropagation();
+  });
+  mobileMenuDOM.append(headerMenu.querySelector("ul").cloneNode(true));
+  headerMenu.append(mobileMenuDOM);
+};
+const closeMenu = () => {
+  mobileMenuDOM.classList.remove("open");
+};
+const openMenu = () => {
+  if (mobileMenuDOM) {
+  } else {
+    creatMobileMenu();
+  }
+  mobileMenuDOM.classList.add("open");
+};
+const toggleMobileMenu = (event) => {
+  if (isMenuOpen) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+  isMenuOpen = !isMenuOpen;
+};
+iconMobile.addEventListener("click", (event) => {
+  event.stopPropagation();
+  toggleMobileMenu();
+});
+
+window.addEventListener("click", (event) => {
+  if (isMenuOpen) {
+    toggleMobileMenu();
+  }
+});
+window.addEventListener("resize", (event) => {
+  if (window.innerWidth > 480 && isMenuOpen) {
+    toggleMobileMenu();
+  }
+});
